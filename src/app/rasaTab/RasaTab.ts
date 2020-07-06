26d7c964b11a48573f2662dc483a7c72c0c80410
Troy <troykirin@gmail.com>
2020-07-06 04:03:54 -0700

    add webchat.html to preventiframe

--- src/app/rasaTab/RasaTab.ts
+++ src/app/rasaTab/RasaTab.ts
@@ -3,8 +3,8 @@ import { PreventIframe } from "express-msteams-host";
 /**
  * Used as place holder for the decorators
  */
-@PreventIframe("/rasaTab/index.html")
-@PreventIframe("/rasaTab/config.html")
-@PreventIframe("/rasaTab/remove.html")
-export class RasaTab {
-}
+@PreventIframe('/rasaTab/index.html')
+@PreventIframe('/rasaTab/config.html')
+@PreventIframe('/rasaTab/remove.html')
+@PreventIframe('/rasaTab/webchat.html')
+export class RasaTab {}
